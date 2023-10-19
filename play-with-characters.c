2b11fc104d33eb0737637b33f10af9efbcbc71d1
Jerlin shabi <138958887+jerlin-shabi@users.noreply.github.com>
2023-10-19 19:17:22 +0530

    play-with-characters.c

---
+++ play-with-characters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch = 'C';
+    char s[] = "Language";
+    char sen[] = "Welcome To C!!";
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+    return 0;
+}
